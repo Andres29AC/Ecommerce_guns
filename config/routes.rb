4cd21cbe7919e99421593358db0449d07f8da67e
Andres Amaro <lionAS_45@hotmail.com>
2023-12-11 04:12:35 -0500

    Avance 2 de tienda de armas config y db

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
-  delete '/productos/:id', to: 'productos#destroy'
-  patch '/productos/:id', to: 'productos#update'
-  post '/productos', to: 'productos#create'
-  get '/productos/new', to: 'productos#new',as: :new_producto
-  get '/productos', to: 'productos#index'
-  get '/productos/:id', to: 'productos#show', as: :producto
-  get '/productos/:id/edit', to: 'productos#edit', as: :edit_producto
+  #delete '/productos/:id', to: 'productos#destroy'
+  #patch '/productos/:id', to: 'productos#update'
+  #post '/productos', to: 'productos#create'
+  #get '/productos/new', to: 'productos#new',as: :new_producto
+  #get '/productos', to: 'productos#index'
+  #get '/productos/:id', to: 'productos#show', as: :producto
+  #get '/productos/:id/edit', to: 'productos#edit', as: :edit_producto
+  resources :productos, path: '/'
 end
 #Cada vez que se haga una peticion se mandara
 #a to: 'productos#index'
@@ -17,3 +18,4 @@ end
 #show es para mostrar un unico producto
 
 #as: sirve para darle un nombre a la ruta
+#
